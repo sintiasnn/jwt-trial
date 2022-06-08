9b72c12f715b65b8fa4f89e6d422fd8c3b966c8e
sintiasnn <sintiawati.putu04@gmail.com>
2022-06-08 16:01:35 +0800

    updating many files

--- app.js
+++ app.js
@@ -1,23 +1,102 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("./model/user");
+const auth = require("./middleware/auth");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 
-// logic goes here
-// importing user context
-const User = require("./model/user");
+
 
 // Register
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
 // our register logic goes here...
+    try{
+        const { first_name, last_name, email, password } = req.body;
+
+        if(!(email&&password&&first_name&&last_name)){
+            res.status(400).send("All input is required");
+        }
+
+        const oldUser= await User.findOne ({ email });
+
+        if (oldUser) {
+            return res.status(409).send("User already exist. please login");
+        }
+
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+        user.token = token;
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // Login
 app.post("/login", (req, res) => {
 // our login logic goes here
+try{
+    const { email, password } = req.body;
+
+    if(!(email && password)) {
+        res.status(400).send("All input is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2hr",
+            }
+        );
+
+        user.token = token;
+
+        res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+} catch (err) {
+    console.log(err);
+}
 });
+
+app.get("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+});
+
+app.use("*", (req, res) => {
+    res.status(404).json({
+      success: "false",
+      message: "Page not found",
+      error: {
+        statusCode: 404,
+        message: "You reached a route that is not defined on this server",
+      },
+    });
+  });
 
 module.exports = app;
